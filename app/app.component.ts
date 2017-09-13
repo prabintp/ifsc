53dc6409e00862be8497d96241103c7c59689fab
prabin tp <prabintp@gmail.com>
2017-09-13 16:07:57 +0530

    space changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES }  from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES, HeaderComponent],
+
+
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  title = '';
+
+
+
+  ngOnInit() {
+  // this.heroService.getHeroes()
+  //   .then(heroes =>  heroes.slice(1, 5));
+    console.log('on init');
+ }
+}
