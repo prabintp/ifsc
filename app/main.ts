6447ca52d3df6a6069eeb35b7e3a6bd12cdb96bc
prabin tp <prabintp@gmail.com>
2017-11-02 16:53:26 +0530

    commit latest changes

--- app/main.ts
+++ app/main.ts
@@ -4,6 +4,7 @@ import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent } from './app.component';
 import { appRouterProviders }   from './app.routes';
 
+
 bootstrap(AppComponent,[
   appRouterProviders,
   HTTP_PROVIDERS
